c91c0b328b94e9a43dcfcf267e0b062d03f4ec60
mphelps1978 <phelps.michael@live.com>
2019-12-20 12:16:39 -0600

    Internet Issues prohibiting me from accessing the API as needed. Created an express API locally, using the React-Rounter-Movies project as a guide. re-hooked up the axios calls to access new local API (localhost:5000/api/characters)

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./character.css";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get("http://localhost:5000/api/characters")
+      .then(response => {
+        console.log("Response: ", response.data[0].results);
+        setCharacter(response.data[0].results);
+        // console.log("character: ", character);
+      })
+      .catch(error => {
+        console.log(`Error :`, error.response);
+      });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {character.map(item => {
+        return (
+          <div key={item.id}>
+            <img src={item.image} />
+            <p>{item.name}</p>
+            <p className="status">{item.status}</p>
+            <p>{item.species}</p>
+            <p>{item.gender}</p>
+          </div>
+        );
+      })}
     </section>
   );
 }
